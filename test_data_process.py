07e2f3590d6e56904b5c1c59ad5fc42cf296be90
leehomjo <64053359+leehomjo@users.noreply.github.com>
2020-09-15 17:46:50 +0800

    Update test_data_process.py

--- test_data_process.py
+++ test_data_process.py
@@ -37,7 +37,7 @@ class GatewayParameters:
         self.recv_rssi = [] #接收信号强度
         self.recv_snr = []  #接收信噪比
         self.recv_len = []  #接收数据长度
-        self.recv_daterate = [] #接收空中速率
+        self.recv_datarate = [] #接收空中速率
         '发送相关'
         self.send_type = [] #发送数据类型
         self.send_freq = [] #发送频点
@@ -46,8 +46,8 @@ class GatewayParameters:
         self.send_len = []  #发送数据长度
         self.send_datarate = [] #发送空中速率
 
-node_para = NodeParameters()
-gw_para = GatewayParameters()
+node_para = NodeParameters()    #节点全局变量
+gw_para = GatewayParameters()   #网关全局变量
 
 class GetInputTxt:
     '创建与输入txt文件相关的类，用于获取要处理的文件名与文件路径'
@@ -85,8 +85,8 @@ class GetInputTxt:
 
     def txt_input(self):
         '手动输入文件路径名和文件名'
-        current_path = 'E:\OKW\自组网\测试'
-        current_file = '节点1-20200911.txt'
+        current_path = '/home/pi/Desktop/python'
+        current_file = '节点1-20200915.txt'
         # current_path = input('请输入log文件存放路径：\n')  # 定义测试log文件存放路径
         # current_file = input('请输入log文件名：\n')  # 定义要处理的log文件名
         txt_type = GetInputTxt.get_current_file(self,current_path, current_file)
@@ -127,18 +127,20 @@ class TxtProcess:
     '对文件的数据处理'
     def module_judgement(self,file_name):
         '判断输入文件是网关数据还是节点数据'
+        txt_name = ''
         txt_split = []  #将路径分割成列表中的独立元素
-        txt_split = file_name.split('\\')    #以文件分隔符分割路径名字符串
+        txt_split = file_name.split('/')    #以文件分隔符分割路径名字符串
         for each in txt_split:
             if each.find('.txt') > 0:
                 txt_name = each
-        txttype = txt_name[0:2]  # 获取字符串前两位
-        if txttype == "节点":
+        if txt_name.find("节点") == 0:
             print('当前处理数据为 %s log数据' % txt_name[0:3])
             return True
-        else:
+        elif txt_name.find("网关") == 0:
             print('当前处理数据为 网关 log数据')
             return False
+        else:
+            pass
 
     def module_process(self,file_name):
         '根据文件名选择处理网关或节点文件'
@@ -148,33 +150,131 @@ class TxtProcess:
             TxtProcess.gateway_process(self,file_name)
 
     def node_process(self,file_name):
-        com_cnt,line_count = 0
+        '节点数据处理函数'
+        com_cnt = [0 for i in range(100)]
+        line_count = 0
         with open(file_name, encoding='utf-8') as node_file:  # 打开文件并定义文件操作类型
             for each_line in node_file.readlines():
-                if each_line.find('通信开始') > 0 and com_cnt == 0:
+                line_count += 1
+                if each_line.find('通信开始') > 0 and com_cnt[0] == 0:
                     com_cnt = 1
                     node_para.com_count.append(com_cnt)
+                    coma[0].append(com_cnt)
+                    print(coma)
                 elif each_line.find('通信开始') > 0 and com_cnt == 1:
                     com_cnt = 2
                     node_para.com_count.append(com_cnt)
-                while com_cnt > 0:
-                    line_count += 1
-                    node_para.com_content.append(each_line)
-        export_txt()
 
-    def node_one_start(self,content):
+
+                # while com_cnt > 0:
+                #     line_count += 1
+                #     node_para.com_content.append(each_line)
+
+    def node_once_recv(self,content):
+        '节点单次接收处理'
         for each in content:
             if each.find('发送数据') > 0:
                 node_para.recv_count.append(each[1])
-                print(node_para.recv_count)
+                # print(node_para.recv_count)
 
-
-    def gateway_process(self,file_name):
+    def node_once_send(self,content):
+        '节点单次发送处理'
         return 0
 
-def export_txt():
-    print(node_para.com_count)
-    print(node_para.com_content)
+    def gateway_process(self,file_name):
+        '网关数据处理函数'
+        send_once_content = []  #单次发送数据内容
+        recv_once_content = []  #单次接收数据内容
+        with open(file_name,encoding='utf-8') as gw_file:
+            for each_line in gw_file.readlines():
+                if each_line.find('TX') == 0:   #发送处理
+                    each_line = each_line[3:]
+                    send_once_content = each_line.split(None)   #以空格作为文件分割条件
+                    TxtProcess.gw_once_send(self,send_once_content)
+                elif each_line.find('RX') == 0: #接收处理
+                    each_line = each_line[3:]
+                    recv_once_content = each_line.split(None)
+                    TxtProcess.gw_once_recv(self,recv_once_content)
+                else:
+                    pass
+            TxtProcess.gateway_export(self) #网关处理数据打印
+
+    def gw_once_recv(self,content):
+        '网关单次接收处理'
+        name = []   #接收名称
+        value = []  #接收参数
+        recv_type = []  #接收类型
+        for each in content:
+            (name,value) = each.split(':',1)    #以冒号为分隔符分割参数
+            if name == 'id':    #接收节点ID号
+                gw_para.recv_node_label.append(value)
+            elif name == 'type':    #接收帧类型
+                (recv_type,value) = value.split('_',1)  #以下划线为分隔符分割帧类型参数
+                gw_para.recv_type.append(recv_type)
+            elif name == 'freq_hz': #接收频点
+                gw_para.recv_freq.append(value)
+            elif name == 'if_chain':    #接收通道号
+                gw_para.recv_channel.append(value)
+            elif name == 'size':    #接收数据长度
+                gw_para.recv_len.append(value)
+            elif name == 'datarate':    #接收空中速率
+                gw_para.recv_datarate.append(value)
+            elif name == 'cnt': #接收帧计数
+                gw_para.recv_count.append(value)
+            elif name == 'sn':  #接收帧序号
+                gw_para.recv_frame_num.append(value)
+            elif name == 'rssi':    #接收信号强度
+                gw_para.recv_rssi.append(value)
+            elif name == 'snr': #接收信噪比
+                gw_para.recv_snr.append(value)
+            else:
+                pass
+
+    def gw_once_send(self,content):
+        '网关单次发送处理'
+        name = []   #发送名称
+        value = []  #发送参数
+        send_type = []  #发送帧类型
+        for each in content:
+            (name, value) = each.split(':', 1)
+            if name == 'freq_hz':   #发送频点
+                gw_para.send_freq.append(value)
+            elif name == 'rf_power':    #发送功率
+                gw_para.send_power.append(value)
+            elif name == 'size':    #发送数据长度
+                gw_para.send_len.append(value)
+            elif name == 'datarate':    #发送空中速率
+                gw_para.send_datarate.append(value)
+            elif name == 'cnt': #发送帧计数
+                gw_para.send_count.append(value)
+            elif name == 'type':    #发送帧类型
+                (send_type,value) = value.split('_',1)
+                gw_para.send_type.append(send_type)
+            else:
+                pass
+
+    def gateway_export(self):
+        print('************send*************')
+        print('count:%s' % gw_para.send_count)
+        print('type:%s' % gw_para.send_type)
+        print('freq:%s' % gw_para.send_freq)
+        print('power:%s' % gw_para.send_power)
+        print('len:%s' % gw_para.send_len)
+        print('datarate:%s' % gw_para.send_datarate)
+        print('*****************************')
+
+        print('***********receive************')
+        print('count:%s' % gw_para.recv_count)
+        print('id:%s' % gw_para.recv_node_label)
+        print('type:%s' % gw_para.recv_type)
+        print('freq:%s' % gw_para.recv_freq)
+        print('datarate:%s' % gw_para.recv_datarate)
+        print('channel:%s' % gw_para.recv_channel)
+        print('frame_num:%s' % gw_para.recv_frame_num)
+        print('len:%s' % gw_para.recv_len)
+        print('rssi:%s' % gw_para.recv_rssi)
+        print('snr:%s' % gw_para.recv_snr)
+        print('*****************************')
 
 def test_process(file_name):
     '对字符型文件按需求进行分割，将所有序列按奇偶进行排序并去除掉首字母序号，将剩下的字符创建新的文件并保存'
@@ -211,6 +311,7 @@ def test_process(file_name):
 if __name__ == '__main__':
     threadtxt = ThreadTxt()
     threadtxt.thread_input()
+
 
 
 
